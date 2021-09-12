c59a933a49945dc24a2851aced6a776a228c859d
Lydie Paulo <lydie.paulo@abcsalles.com>
2021-09-13 00:09:41 +0200

    [fix] Navlinks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,18 +12,15 @@ const Navbar = () => {
     <nav className="nav" id="nav">
       <ul className="nav-bar">
         <li className="nav-bar__home">
-          <NavLink exact to="/lydie-paulo-paris-event
-Public">
+          <NavLink exact to="/">
             <img src={favicon} alt="favicon" className="logo" />
           </NavLink>
         </li>
         <li>
-          <NavLink exact to="/lydie-paulo-paris-event
-Public/search" activeClassName="nav-bar__active">Rechercher</NavLink>
+          <NavLink exact to="/search" activeClassName="nav-bar__active">Rechercher</NavLink>
         </li>
         <li>
-          <NavLink exact to="/lydie-paulo-paris-event
-Public/favorites" activeClassName="nav-bar__active">Favoris</NavLink>
+          <NavLink exact to="/favorites" activeClassName="nav-bar__active">Favoris</NavLink>
         </li>
       </ul>
     </nav>
